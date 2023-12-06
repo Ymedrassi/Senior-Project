29c469db60c16b92a312baffec23a939f29406db
Abdulmjed <mjoode75@gmail.com>
2023-12-06 03:34:19 +0300

    Create custum control

---
+++ CirclePanel.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Windows.Forms
+
+Public Class CircularPanel
+    Inherits Panel
+
+    Public Sub New()
+        ' Set some initial properties
+        Me.DoubleBuffered = True
+        Me.BackColor = Color.Transparent
+    End Sub
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        Using g As Graphics = e.Graphics
+            ' Create a circular path
+            Dim path As New GraphicsPath()
+            path.AddEllipse(ClientRectangle)
+
+            ' Set the panel's region to the circular path
+            Me.Region = New Region(path)
+
+            ' Fill the circular panel with a color or image
+            Dim fillColor As Color = Color.LightBlue ' Change this to your desired fill color
+            Using fillBrush As New SolidBrush(fillColor)
+                g.FillPath(fillBrush, path)
+            End Using
+
+            ' Optionally, you can draw a border around the circular panel
+            Dim borderColor As Color = Color.DarkBlue ' Change this to your desired border color
+            Using borderPen As New Pen(borderColor, 2) ' Change the width if needed
+                g.DrawPath(borderPen, path)
+            End Using
+        End Using
+    End Sub
+End Class
---
+++ CustomSwitch.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+
+Public Class CustomSwitch
+    Inherits Control
+
+    Private isOn As Boolean = False
+
+    Public Event SwitchStateChanged(sender As Object, e As EventArgs)
+
+    Public Sub New()
+        Me.Size = New Size(80, 40)
+        Me.BackColor = Color.Gray
+        Me.DoubleBuffered = True
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Protected Overrides Sub OnClick(e As EventArgs)
+        MyBase.OnClick(e)
+        ToggleSwitch()
+    End Sub
+
+    Private Sub ToggleSwitch()
+        isOn = Not isOn
+        Me.Invalidate()
+        RaiseEvent SwitchStateChanged(Me, EventArgs.Empty)
+
+    End Sub
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        Using g As Graphics = e.Graphics
+            ' Draw the background
+            Dim bgColor As Color = If(isOn, Color.Green, Color.Gray)
+            Using bgBrush As New SolidBrush(bgColor)
+                g.FillRectangle(bgBrush, ClientRectangle)
+            End Using
+
+            ' Draw the switch button
+            Dim buttonColor As Color = Color.White
+            Dim buttonX As Integer = If(isOn, Width - Height, 0)
+            Using buttonBrush As New SolidBrush(buttonColor)
+                g.FillEllipse(buttonBrush, New Rectangle(buttonX, 0, Height, Height))
+            End Using
+        End Using
+    End Sub
+    Public Property IsOnState() As Boolean
+        Get
+            Return isOn
+        End Get
+        Set(value As Boolean)
+            If isOn <> value Then
+                isOn = value
+                Me.Invalidate() ' Redraw the control
+            End If
+        End Set
+    End Property
+
+End Class
