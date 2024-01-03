13189e563f639d58abd96ea11befec2250f01206
moe5663 <m.7.bananas1422@gmail.com>
2024-01-04 02:52:31 +0300

    new change protection by moe5663

---
+++ CtlProtection.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CtlProtection
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.indicationImage = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.indicationImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Label7.Location = New System.Drawing.Point(62, 66)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(198, 58)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Firewall"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Location = New System.Drawing.Point(88, 505)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(526, 120)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(63, 45)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(234, 35)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Domain Network"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Anti_Virus_Project.My.Resources.Resources.off
+        Me.PictureBox2.Location = New System.Drawing.Point(358, 25)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(148, 74)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 13
+        Me.PictureBox2.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Label2.Location = New System.Drawing.Point(88, 462)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(360, 29)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Domain Network (Recomended)"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Label1.Location = New System.Drawing.Point(652, 462)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(351, 29)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Private Network (Recomended)"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.PictureBox3)
+        Me.Panel2.Location = New System.Drawing.Point(658, 505)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(526, 120)
+        Me.Panel2.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Black
+        Me.Label6.Location = New System.Drawing.Point(51, 45)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(223, 35)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Private Network"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Anti_Virus_Project.My.Resources.Resources.off
+        Me.PictureBox3.Location = New System.Drawing.Point(352, 25)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(148, 74)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 14
+        Me.PictureBox3.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Label3.Location = New System.Drawing.Point(458, 357)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(522, 58)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "is NOT protecting you!"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Orange
+        Me.Label4.Location = New System.Drawing.Point(264, 357)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(195, 58)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Scanvy"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(1162, 634)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(22, 21)
+        Me.CheckBox1.TabIndex = 13
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CheckBox1.Visible = False
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(592, 634)
+        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(22, 21)
+        Me.CheckBox2.TabIndex = 14
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.CheckBox2.Visible = False
+        '
+        'indicationImage
+        '
+        Me.indicationImage.Image = Global.Anti_Virus_Project.My.Resources.Resources.icons8_warning_shield_96px
+        Me.indicationImage.Location = New System.Drawing.Point(549, 166)
+        Me.indicationImage.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.indicationImage.Name = "indicationImage"
+        Me.indicationImage.Size = New System.Drawing.Size(172, 157)
+        Me.indicationImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.indicationImage.TabIndex = 12
+        Me.indicationImage.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Anti_Virus_Project.My.Resources.Resources.icons8_info_45px
+        Me.PictureBox4.Location = New System.Drawing.Point(262, 82)
+        Me.PictureBox4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(30, 31)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox4.TabIndex = 6
+        Me.PictureBox4.TabStop = False
+        '
+        'CtlProtection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(90, Byte), Integer))
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.indicationImage)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.Label7)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "CtlProtection"
+        Me.Size = New System.Drawing.Size(1272, 871)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.indicationImage, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents indicationImage As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ CtlProtection.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Net.NetworkInformation
+Public Class CtlProtection
+
+    ' Enable a network adapter
+    Public Shared Sub EnableNetworkAdapter(adapterName As String)
+        Try
+            Dim networkInterface As NetworkInterface = GetNetworkInterfaceByName(adapterName)
+            If networkInterface IsNot Nothing Then
+                'networkInterface.Enable()
+                Console.WriteLine($"Enabled network adapter: {adapterName}")
+            Else
+                Console.WriteLine($"Network adapter '{adapterName}' not found.")
+            End If
+        Catch ex As Exception
+            Console.WriteLine($"Error enabling network adapter: {ex.Message}")
+        End Try
+    End Sub
+
+    ' Disable a network adapter
+    Public Shared Sub DisableNetworkAdapter(adapterName As String)
+        Try
+            Dim networkInterface As NetworkInterface = GetNetworkInterfaceByName(adapterName)
+            If networkInterface IsNot Nothing Then
+                'networkInterface.Disable()
+                Console.WriteLine($"Disabled network adapter: {adapterName}")
+            Else
+                Console.WriteLine($"Network adapter '{adapterName}' not found.")
+            End If
+        Catch ex As Exception
+            Console.WriteLine($"Error disabling network adapter: {ex.Message}")
+        End Try
+    End Sub
+
+    ' Get a network interface by name
+    Private Shared Function GetNetworkInterfaceByName(adapterName As String) As NetworkInterface
+        Dim networkInterfaces As NetworkInterface() = NetworkInterface.GetAllNetworkInterfaces()
+        For Each networkInterface As NetworkInterface In networkInterfaces
+            If networkInterface.Name = adapterName Then
+                Return networkInterface
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked Then
+            PictureBox2.Image = My.Resources._ON
+            PictureBox3.Image = My.Resources._ON
+            Label3.Text = "is protecting you!"
+            ' Enable the network adapter here
+            EnableNetworkAdapter("YourNetworkAdapterName")
+            My.Settings.protection = True
+            My.Settings.Save()
+        Else
+            PictureBox2.Image = My.Resources.off
+            PictureBox3.Image = My.Resources.off
+            Label3.Text = "is NOT protecting you!"
+            My.Settings.protection = False
+            My.Settings.Save()
+            ' Disable the network adapter here
+            DisableNetworkAdapter("YourNetworkAdapterName")
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            PictureBox2.Image = My.Resources._ON
+            PictureBox3.Image = My.Resources._ON
+            Label3.Text = "is protecting you!"
+            ' Enable the network adapter here
+            EnableNetworkAdapter("YourNetworkAdapterName")
+            My.Settings.protection = True
+            My.Settings.Save()
+        Else
+            PictureBox2.Image = My.Resources.off
+            PictureBox3.Image = My.Resources.off
+            Label3.Text = "is NOT protecting you!"
+            My.Settings.protection = False
+            My.Settings.Save()
+            ' Disable the network adapter here
+            DisableNetworkAdapter("YourNetworkAdapterName")
+        End If
+    End Sub
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        If CheckBox2.Checked = False Then
+            CheckBox2.Checked = True
+
+        Else
+            CheckBox2.Checked = False
+        End If
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        If CheckBox1.Checked = False Then
+            CheckBox1.Checked = True
+        Else
+            CheckBox1.Checked = False
+        End If
+    End Sub
+
+    Private Sub CtlProtection_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ChangeLabelForeColors(My.Settings.foreColor)
+        If My.Settings.protection = True Then
+            CheckBox1.Checked = True
+            CheckBox2.Checked = True
+        Else
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+        End If
+    End Sub
+    Private Sub ChangeLabelForeColors(ByVal newColor As Color)
+        For Each control As Control In Me.Controls
+            If TypeOf control Is Label Then
+                Dim labelControl As Label = DirectCast(control, Label)
+                labelControl.ForeColor = newColor
+            End If
+        Next
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
